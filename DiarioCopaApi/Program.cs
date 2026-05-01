b61928794c5f795319731a4c969e689a96d750cf
gabdourado <gab.dourado.if23@gmail.com>
2026-05-01 15:20:10 -0300

    feat: adicionando migrate automático

--- DiarioCopaApi/Program.cs
+++ DiarioCopaApi/Program.cs
@@ -16,6 +16,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DiarioCopaContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
