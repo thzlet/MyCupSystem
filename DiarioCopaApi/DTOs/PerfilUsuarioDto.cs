fab608817a2f0b06fd507607907f75bbee0cea91
IarleyF <iarleyicapui2016@gmail.com>
2026-05-29 14:44:15 -0300

    feature: usuarios/perfil

---
+++ DiarioCopaApi/DTOs/PerfilUsuarioDto.cs
@@ -0,0 +1,8 @@
+namespace DiarioCopaApi.DTOs;
+
+public class PerfilUsuarioDto
+{
+    public Guid Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
